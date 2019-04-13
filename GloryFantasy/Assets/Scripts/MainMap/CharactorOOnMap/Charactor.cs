e5f04fd0fb3143ca7a44b4a1f8ee50a419148fe0
xqqxiaoqiqi <1577232763@qq.com>
2019-04-13 22:21:47 +0800

    完善大地图
    现在驿站传送还有一点点问题，还在修改

--- GloryFantasy/Assets/Scripts/MainMap/CharactorOOnMap/Charactor.cs
+++ GloryFantasy/Assets/Scripts/MainMap/CharactorOOnMap/Charactor.cs
@@ -1,62 +1,130 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using System;
 
-//2019.4.10  13：50版
 
-public class Charactor : MonoBehaviour {
+//2019.4.12版
+
+public class Charactor : MonoBehaviour
+{
     public Vector3 locate;
+    /// <summary>开放给策划用于在Unity的Inspector里直接调整数值，代码内需要修改角色信息请修改CharacorData.
+    /// 
+    /// </summary>
     public int HP;//人物血量
+    public int Step;//当前剩余步数，貌似不需要在外部修改，写在外面只是为了看见它。
     public int MaxStep;//最大步数
-    public Charactor(int hp,int maxstep)
-        {
-        this.HP = hp;
-        this.MaxStep = maxstep;
-        }
+    /// <summary>储存角色信息的数据结构，考虑到未来可能有需求需要返回全部的数值，就写了这么个玩意。
+    /// 
+    /// </summary>
     public struct CharactorData
     {
-        public  int HP ;
-        public  int MaxStep ;
-        public  object[,] Bag ;
-        public Transform mytransform ;
-       
+        public int HP;
+        public int MaxStep;
+        public int Step;
+        public object[,] Bag;
+        public HexVector PlayerLocate;
+        public GameObject UnderFeet;
     }
-    public CharactorData charactorData= new CharactorData();
-    public Vector3 Initialize(Vector3 locate)
+    public MainMapManager mapManager;
+    public CharactorData charactorData;
+    /// <summary>设定角色初始位置
+    /// 
+    /// </summary>
+    /// <param name="locate"></param>
+    /// <returns></returns>
+    public Vector3 SetCharactorLocate(Vector3 locate)
     {
-        Debug.Log("初始化角色坐标");
-       this.locate.x = locate.x;
-       this.locate.y = locate.y;
-       this.locate.z = locate.z;
-       charactorData.mytransform.position = locate;
+
+        Debug.Log("初始化角色位置");
+        charactorData.PlayerLocate.Normal_vector = locate;
         Debug.Log("Initialize complete!" + locate);
-        return charactorData.mytransform.position;
+        return charactorData.PlayerLocate.Normal_vector;
     }
-    public CharactorData Initialize(int hp, int maxstep)
+    /// <summary>初始化角色信息
+    /// 
+    /// </summary>
+    public void Initalize()
     {
-        Debug.Log("初始化角色最大步数与血量");
+        this.SetCharactorLocate(locate);
+        this.SetMessage(HP, MaxStep);
+        GetComponent<Transform>().position = locate;
+        charactorData.PlayerLocate.Normal_vector = transform.GetComponent<Transform>().position;
+        charactorData.UnderFeet = GameObject.Find("test" + charactorData.PlayerLocate.ChangeToHexVect(charactorData.PlayerLocate.Normal_vector).x.ToString() + charactorData.PlayerLocate.ChangeToHexVect(charactorData.PlayerLocate.Normal_vector).z.ToString());
+        mapManager.setaround(charactorData.UnderFeet);
+        Debug.Log("角色初始化完成");
+
+    }
+    /// <summary>设定初始步数和血量
+    /// 
+    /// </summary>
+    /// <param name="hp"></param>
+    /// <param name="maxstep"></param>
+    /// <returns></returns>
+    public CharactorData SetMessage(int hp, int maxstep)
+    {
+        Debug.Log("初始化角色最大步数和血量");
         charactorData.HP = hp;
         charactorData.MaxStep = maxstep;
-        Debug.Log("Initialize complete HP:" + charactorData.HP + " Maxstep:" + charactorData.MaxStep);
+        charactorData.Step = charactorData.MaxStep;
+        Step = charactorData.Step;
+        Debug.Log("Initialize complete HP:" + charactorData.HP + " Maxstep:" + charactorData.Step);
         return charactorData;
     }
-    public Vector3 Move(Vector3 locate)
+    /// <summary>如果移动合法，调用这个函数改变角色坐标。
+    /// 
+    /// </summary>
+    /// <param name="newtransform"></param>
+    /// <returns></returns>
+    public Vector3 Move(Vector3 newtransform, int step)
     {
-        this.charactorData.mytransform.position = locate;
-        return locate;
+        this.charactorData.PlayerLocate.Normal_vector = newtransform;
+        this.GetComponent<Transform>().position = charactorData.PlayerLocate.Normal_vector;
+        charactorData.UnderFeet = GameObject.Find("test" + charactorData.PlayerLocate.ChangeToHexVect(charactorData.PlayerLocate.Normal_vector).x.ToString() + charactorData.PlayerLocate.ChangeToHexVect(charactorData.PlayerLocate.Normal_vector).z.ToString());
+        this.ChangeStep(step);
+        return newtransform;
     }
-	// Use this for initialization
-	void Start () {
-        
-        charactorData.mytransform = transform.GetComponent<Transform>();
-        this.Initialize(locate);
-        this.Initialize(HP, MaxStep);
-       
+
+    /// <summary>改变角色步数时调用， 并返回剩余步数。
+    /// 
+    /// </summary>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public int ChangeStep(int value)
+    {
+        if (charactorData.Step + value <= charactorData.MaxStep)
+        {
+
+            charactorData.Step = charactorData.Step + value;
+            Step = charactorData.Step;
+            if (charactorData.Step < 0)
+            {
+                HasDead();
+            }
+        }
+        else
+        {
+            Debug.Log("超出最大步数");
+            charactorData.Step = charactorData.MaxStep;
+            Step = charactorData.Step;
+
+        }
+
+        return charactorData.Step;
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    /// <summary>角色死亡时调用。
+    /// 
+    /// </summary>
+    public void HasDead()
+    {
+
+        Debug.Log("步数为负，角色累死了，返回起点");
+        Initalize();
+
+    }
+    // Use this for initialization
+    void Awake()
+    {
+
+        mapManager = GameObject.Find("Map").GetComponent<MainMapManager>();
+    }
 }
---
+++ GloryFantasy/Assets/Scripts/MainMap/MainMapManager.cs
@@ -0,0 +1,273 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+/// <summary>定义六边形坐标的结构体，并处理坐标转换
+/// 
+/// </summary>
+public struct HexVector
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public Vector3 Hex_vector;
+    /// <summary>世界坐标
+    /// 
+    /// </summary>
+    public Vector3 Normal_vector;
+    public Vector3 ChangeToNormalVect(Vector3 vector)
+    {
+        //  HexVector hexVector = new HexVector();
+        Normal_vector.x = 1.5f * vector.x + 1.5f * vector.z;
+        Normal_vector.y = vector.y;
+        Normal_vector.z = -vector.x + vector.z;
+        vector.x = Normal_vector.x;
+        vector = Normal_vector;
+        return vector;
+
+    }
+    public Vector3 ChangeToHexVect(Vector3 vector)
+    {
+        Hex_vector.x = vector.x / 3f - 0.5f * vector.z;
+        Hex_vector.y = vector.y;
+        Hex_vector.z = vector.x / 3f + 0.5f * vector.z;
+        vector = Hex_vector;
+        return vector;
+    }
+
+}
+/// <summary>在这个类里读取地图文件并生成地图
+/// 
+/// </summary>
+public class MainMapManager : MonoBehaviour
+{
+    /// <summary>地格材质，测试用，运行的时候找一个Unity默认的材质加上去就行。
+    /// 
+    /// </summary>
+    public Mesh mesh;
+    /// <summary>人物角色实例。
+    /// 
+    /// </summary>
+    public Charactor charactor;
+    /// <summary>角色起始所在位置。
+    /// 
+    /// </summary>
+    /// <summary>储存角色周围地格信息的数据结构
+    ///
+    /// </summary>
+    public Dictionary<string, MapUnit> AroundList = new Dictionary<string, MapUnit>();
+    /// <summary>初始化，设定
+    /// 
+    /// </summary>
+    void Awake()
+    {
+        AroundList.Add("0,1", null);
+        AroundList.Add("0,-1", null);
+        AroundList.Add("1,0", null);
+        AroundList.Add("-1,0", null);
+        AroundList.Add("-1,1", null);
+        AroundList.Add("1,-1", null);
+        //测试用
+        string[,] simtext = new string[3, 3]
+        {
+            {"plane","plane","post" },
+            { "plane","plane","plane"},
+            {"post","plane","plane" },
+
+        };
+
+        for (int i = 0; i < simtext.GetLength(0); i++)
+        {
+            for (int j = 0; j < simtext.GetLength(1); j++)
+            {
+                GameObject gameObject = new GameObject("test" + i.ToString() + j.ToString());
+                gameObject.transform.parent = GameObject.Find("Map").transform;
+                HexVector vector = new HexVector();
+                gameObject.AddComponent<Button>();
+                gameObject.AddComponent<MapUnit>();
+                MeshFilter flit = gameObject.AddComponent<MeshFilter>();
+                gameObject.AddComponent<MeshRenderer>();
+                gameObject.AddComponent<BoxCollider>();
+                flit.mesh = mesh;
+                gameObject.transform.position = vector.ChangeToNormalVect(new Vector3(i, 0, j));
+                gameObject.GetComponent<MapUnit>().SethexVector();
+                switch (simtext[i, j])
+                {
+                    case "plane":
+                        break;
+                    case "post":
+                        gameObject.AddComponent<Post>();
+                        break;
+                }
+                //   Debug.Log(gameObject.transform.position);
+            }
+
+        }
+
+
+
+
+    }
+    private void Start()
+    {
+        charactor = GameObject.Find("Charactor").GetComponent<Charactor>();
+        charactor.Initalize();
+
+    }
+    /// <summary>移动逻辑合法后重设角色周围地形格写入AroundList
+    /// 
+    /// </summary>
+    /// <param name="onclk"></param>
+    /// <returns></returns>
+    public Dictionary<string, MapUnit> setaround(GameObject onclk)
+    {
+
+        AroundList["0,1"] = SetAround(onclk, 0, 1);
+        AroundList["0,-1"] = SetAround(onclk, 0, -1);
+        AroundList["1,0"] = SetAround(onclk, 1, 0);
+        AroundList["-1,0"] = SetAround(onclk, -1, 0);
+        AroundList["-1,1"] = SetAround(onclk, -1, 1);
+        AroundList["1,-1"] = SetAround(onclk, 1, -1);
+
+        return AroundList;
+    }
+
+    // Debug.Log("playeraround");
+
+    /// <summary>重设地形格字典值的具体逻辑
+    /// 
+    /// </summary>
+    /// <param name="onclk"></param>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public MapUnit SetAround(GameObject onclk, float a, float b)
+    {
+        MapUnit playeraround;
+        float x = onclk.GetComponent<MapUnit>().hexVector.Hex_vector.x + a;
+        // float x = onclk.GetComponent<Transform>().position.x+1;
+        float z = onclk.GetComponent<MapUnit>().hexVector.Hex_vector.z + b;
+        if (GameObject.Find("test" + x.ToString() + z.ToString()) != null)
+        {
+            playeraround = GameObject.Find("test" + x.ToString() + z.ToString()).GetComponent<MapUnit>();
+        }
+        else
+
+        {
+            Debug.Log("doesn't have this object");
+            playeraround = null;
+        }
+        return playeraround;
+
+
+    }
+
+
+
+}
+/// <summary>六边形单元格,每个地格都会挂载这个脚本。
+/// 
+/// </summary>
+public class MapUnit : MonoBehaviour
+{
+    public static MainMapManager mapManager;
+    /// <summary>储存地格坐标的结构体，注意地格自身的世界坐标（Vector3 Normal_vector）也在这个结构里。
+    /// 
+    /// </summary>
+    /// 
+    public HexVector hexVector = new HexVector();
+    /// <summary>初始化，给所在GameObject添加Button组件，并把自己在两个坐标系的坐标写入HexVector里。
+    /// 
+    /// </summary>  
+    public void Awake()
+    {
+        mapManager = gameObject.GetComponentInParent<MainMapManager>();
+        Button btn = gameObject.GetComponent<Button>();
+        btn.onClick.AddListener(CheckAround);
+
+        Debug.Log("instalize mapunit.");
+
+    }
+    public HexVector SethexVector()
+    {
+        hexVector.Normal_vector = this.GetComponent<Transform>().position;
+        hexVector.Hex_vector = hexVector.ChangeToHexVect(hexVector.Normal_vector);
+        return hexVector;
+    }
+    /// <summary>检测被点击的地格是否在角色相邻区域，
+    /// 
+    /// </summary>
+    public void CheckAround()
+    {
+        if (mapManager.AroundList.ContainsValue(this))
+        {
+            this.DoOnclick();
+        }
+        else
+        {
+            Debug.Log("这个格子不在角色相邻区域，无法移动");
+        }
+    }
+    /// <summary>点击合法后，大地图初始化或角色死亡后调用此函数，传到MapManager的SetAround来处理。
+    /// 
+    /// </summary>
+    public void DoOnclick()
+    {
+
+        //  hexVector.ChangeToHexVect(hexVector.Normal_vector);
+        mapManager.charactor.Move(GetComponent<Transform>().position, -1);
+        mapManager.setaround(GameObject.Find("test" + mapManager.charactor.charactorData.PlayerLocate.Hex_vector.x.ToString() + mapManager.charactor.charactorData.PlayerLocate.Hex_vector.z.ToString()));
+        Debug.Log("Onclick" + hexVector.Normal_vector.x.ToString() + hexVector.Normal_vector.z.ToString());
+
+    }
+
+
+}
+public class Post : MapUnit
+{
+    private bool isActive = false;
+    private static bool ReadyToTrans = false;
+    public new void Awake()
+    {
+        Debug.Log("驿站初始化");
+        Button btn = gameObject.GetComponent<Button>();
+        btn.onClick.AddListener(Onclick);
+    }
+    public void Onclick()
+    {
+        if (mapManager.charactor.charactorData.PlayerLocate.Normal_vector == GetComponent<Transform>().position && ReadyToTrans == false)
+        {
+            isActive = true;
+            Debug.Log("驿站已激活");
+            Debug.Log("进入驿站");
+            ReadyToTrans = true;
+            Debug.Log("准备传送");
+
+        }
+        else if (ReadyToTrans == true)
+        {
+
+            if (isActive == false)
+            {
+                Debug.Log("所选驿站未激活。");
+            }
+            else
+            {
+                Debug.Log("指令合法，开始传送");
+                transfer();
+                ReadyToTrans = false;
+                Debug.Log("传送完成");
+            }
+        }
+        else
+        {
+            Debug.Log("你不在这个驿站");
+        }
+    }
+    public void transfer()
+    {
+        mapManager.charactor.Move(GetComponent<Transform>().position, -2);
+        mapManager.setaround(GameObject.Find("test" + mapManager.charactor.charactorData.PlayerLocate.Hex_vector.x.ToString() + mapManager.charactor.charactorData.PlayerLocate.Hex_vector.z.ToString()));
+        ReadyToTrans = false;
+    }
+}
