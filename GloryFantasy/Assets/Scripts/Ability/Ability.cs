e072290522b30326d0cefb820e9f4d9c5cc3a3a6
keven2148 <33781038+keven2148@users.noreply.github.com>
2019-04-17 19:20:17 +0800

    增加Abillity数据库的读取；增加序列化和反序列化方法
    想要被序列化需要标注可序列化属性

---
+++ GloryFantasy/Assets/Scripts/Ability/Ability.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using LitJson;
+using Serializer;
+using System.IO;
+
+namespace Ability
+{
+    [Serializable]
+    /// <summary>
+    /// 异能变量类
+    /// </summary>
+    public class AbilityVariable
+    {
+        /// <summary>
+        /// 距离
+        /// </summary>
+        public Nullable<Int32> Range;
+        /// <summary>
+        /// 伤害量
+        /// </summary>
+        public Nullable<Int32> Damage;
+        /// <summary>
+        /// 数目
+        /// </summary>
+        public Nullable<Int32> Amount;
+        /// <summary>
+        /// 抽牌数
+        /// </summary>
+        public Nullable<Int32> Draws;
+        /// <summary>
+        /// 回合数
+        /// </summary>
+        public Nullable<Int32> Turns;
+        /// <summary>
+        /// 范围大小
+        /// </summary>
+        public Nullable<Vector2> Area;
+        /// <summary>
+        /// 治疗量
+        /// </summary>
+        public Nullable<Int32> Curing;
+    }
+
+    /// <summary>
+    /// 异能对象类型枚举
+    /// </summary>
+    public enum TargetType
+    {
+        Enemy,
+        Friendly,
+        Field,
+        All
+    };
+
+    [Serializable]
+    /// <summary>
+    /// 异能对象类
+    /// </summary>
+    public class AbilityTarget
+    {
+        public TargetType TargetType = TargetType.All;
+        public bool IsSpeller = false;
+        public bool IsTarget = false;
+
+        public AbilityTarget(string _targetType, bool _isSpeller, bool _isTarget)
+        {
+            TargetType targetType = TargetType.All;
+            switch (_targetType)
+            {
+                case ("敌人"): targetType = TargetType.Enemy; break;
+                case ("友军"): targetType = TargetType.Friendly; break;
+                case ("地形"): targetType = TargetType.Field; break;
+                case ("所有单位"): targetType = TargetType.All; break;
+            }
+            this.TargetType = targetType;
+            this.IsSpeller = _isSpeller;
+            this.IsTarget = _isTarget;
+        }
+
+        public AbilityTarget(TargetType _targetType, bool _isSpeller, bool _isTarget)
+        {
+            this.TargetType = _targetType;
+            this.IsSpeller = _isSpeller;
+            this.IsTarget = _isTarget;
+        }
+    }
+    /// <summary>
+    /// 异能在数据库的存储格式
+    /// </summary>
+    public class AbilityFormat
+    {
+        /// <summary>
+        /// 对象列表
+        /// </summary>
+        public List<AbilityTarget> AbilityTargetList;
+        /// <summary>
+        /// 异能可用变量
+        /// </summary>
+        public AbilityVariable AbilityVariable;
+
+        /// <summary>
+        /// 异能ID
+        /// </summary>
+        public string AbilityID;
+        /// <summary>
+        /// 组号
+        /// </summary>
+        public int Group;
+        /// <summary>
+        /// 异能中文名
+        /// </summary>
+        /// <param name="_abilityID"></param>
+        public string AbilityName;
+        /// <summary>
+        /// 异能描述
+        /// </summary>
+        public string Description;
+        /// <summary>
+        /// 异能TriggerID
+        /// </summary>
+        /// <param name="_abilityID"></param>
+        public string TriggerID;
+
+        public AbilityFormat(string _abilityID)
+        {
+            AbilityID = _abilityID;
+            AbilityTargetList = new List<AbilityTarget>();
+            AbilityVariable = new AbilityVariable();
+        }
+
+    }
+
+    public class Ability : MonoBehaviour, GameplayTool
+    {
+        /// <summary>
+        /// 对象列表
+        /// </summary>
+        public List<AbilityTarget> AbilityTargetList;
+        /// <summary>
+        /// 异能可用变量
+        /// </summary>
+        public AbilityVariable AbilityVariable;
+
+        /// <summary>
+        /// 异能ID
+        /// </summary>
+        public string AbilityID;
+        /// <summary>
+        /// 组号
+        /// </summary>
+        public int Group;
+        /// <summary>
+        /// 异能中文名
+        /// </summary>
+        /// <param name="_abilityID"></param>
+        public string AbilityName;
+        /// <summary>
+        /// 异能描述
+        /// </summary>
+        public string Description;
+        /// <summary>
+        /// 异能TriggerID
+        /// </summary>
+        /// <param name="_abilityID"></param>
+        public string TriggerID;
+
+        void Awake()
+        {
+            AbilityTargetList = new List<AbilityTarget>();
+        }
+
+        void InitialAbility(string AbilityID)
+        {
+            AbilityFormat abilityFormat = AbilityDatabase.GetInstance().GetAbilityFormat(AbilityID);
+
+            //用序列化拷贝AbilityTargetList;
+            Stream stream = Serializer.Serializer.InstanceDataToMemory(abilityFormat.AbilityTargetList);
+            this.AbilityTargetList = (List<AbilityTarget>)Serializer.Serializer.MemoryToInstanceData(stream);
+            //用序列化拷贝AbilityVariable
+            stream = Serializer.Serializer.InstanceDataToMemory(abilityFormat.AbilityVariable);
+            this.AbilityVariable = (AbilityVariable) Serializer.Serializer.MemoryToInstanceData(stream);
+            //拷贝变量
+            this.AbilityID = abilityFormat.AbilityID;
+            this.Group = abilityFormat.Group;
+            this.AbilityName = abilityFormat.AbilityName;
+            this.Description = abilityFormat.Description;
+            this.TriggerID = abilityFormat.TriggerID;
+        }
+    }
+}
---
+++ GloryFantasy/Assets/Scripts/Serializer/Serializer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+namespace Serializer
+{
+    public static class Serializer
+    {
+        public static MemoryStream InstanceDataToMemory(object instance)
+        {
+            //创建一个新的流来容纳经过序列化的对象
+            MemoryStream memoStream = new MemoryStream();
+            //创建一个序列化格式化器来执行具体的序列化操作
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            //将传入的对象instance序列化到流memoStream中
+            binaryFormatter.Serialize(memoStream, instance);
+            //返回序列化好的流
+            return memoStream;
+        }
+
+        public static object MemoryToInstanceData(Stream memoryStream)
+        {
+            //创建一个序列化格式化器来执行具体的反序列化操作
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            //返回从流memoryStream中反序列化得到的对象
+            return binaryFormatter.Deserialize(memoryStream);
+        }
+    }
+}
