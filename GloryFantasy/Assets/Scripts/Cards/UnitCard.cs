c8558de1620e30f7577c8f52930a32f5636bca17
RookiePC <paco_liao@163.com>
2019-04-18 20:47:20 +0800

    修复CardManager管理卡牌槽的小漏洞

--- GloryFantasy/Assets/Scripts/Cards/UnitCard.cs
+++ GloryFantasy/Assets/Scripts/Cards/UnitCard.cs
@@ -5,6 +5,13 @@ namespace GameUnit
     /// </summary>
     public class UnitCard : BaseCard
     {
-        
+        /// <summary>
+        /// 点击使用卡牌时调用的函数
+        /// </summary>
+        /// <returns>若成功使用则返回true，中途取消或其他情况返回false</returns>
+        public bool Use()
+        {
+            return true;
+        }
     }
 }
