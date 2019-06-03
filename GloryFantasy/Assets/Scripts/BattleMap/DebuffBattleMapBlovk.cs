3860ad00f35998f919e06f7c8464c8f3ccc83ebe
zhubide <651861465@qq.com>
2019-06-03 18:29:13 +0800

    异常地图块完善

--- GloryFantasy/Assets/Scripts/BattleMap/DebuffBattleMapBlovk.cs
+++ GloryFantasy/Assets/Scripts/BattleMap/DebuffBattleMapBlovk.cs
@@ -11,34 +11,47 @@ namespace BattleMap
 {
     public class DebuffBattleMapBlovk
     {
-        //创造灼烧块
-        public void SetBattleMapBlockBurning(List<Vector2> vector2s)
+        /// <summary>
+        /// 创造灼烧块
+        /// </summary>
+        /// <param name="vector2"></param>
+        public void SetBattleMapBlockBurning(Vector2 vector2)
         {
             BattleMapBlock bm = null;
-            foreach (Vector2 vector2 in vector2s)
-            {
-                bm = BattleMap.Instance().GetSpecificMapBlock(vector2);
-                bm.blockType = EMapBlockType.Burnning;
-                SpriteRenderer upLayer = bm.transform.Find("upLayer").GetComponent<SpriteRenderer>();
-                upLayer.sprite = BattleMap.Instance().firing;
-                upLayer.color = new Color(255, 255, 255, 255);
-            }
+            bm = BattleMap.Instance().GetSpecificMapBlock(vector2);
+            bm.blockType = EMapBlockType.Burnning;
+            SpriteRenderer upLayer = bm.transform.Find("upLayer").GetComponent<SpriteRenderer>();
+            upLayer.sprite = BattleMap.Instance().firing;
+            upLayer.color = new Color(255, 255, 255, 255);
         }
 
-        //创造滞留块
-        public void SetBattleMapBlockRetrad(List<Vector2> vector2s)
+        /// <summary>
+        /// 创造滞留块
+        /// </summary>
+        /// <param name="vector2s"></param>
+        public void SetBattleMapBlockRetrad(Vector2 vector2)
         {
             BattleMapBlock bm = null;
-            foreach (Vector2 vector2 in vector2s)
-            {
-                bm = BattleMap.Instance().GetSpecificMapBlock(vector2);
-                bm.blockType = EMapBlockType.Retire;
-                SpriteRenderer upLayer = bm.transform.Find("upLayer").GetComponent<SpriteRenderer>();
-                upLayer.sprite = BattleMap.Instance().viscous;
-                upLayer.color = new Color(255, 255, 255, 255);
-            }
+            bm = BattleMap.Instance().GetSpecificMapBlock(vector2);
+            bm.blockType = EMapBlockType.Retire;
+            SpriteRenderer upLayer = bm.transform.Find("upLayer").GetComponent<SpriteRenderer>();
+            upLayer.sprite = BattleMap.Instance().viscous;
+            upLayer.color = new Color(255, 255, 255, 255);
         }
-        
+
+        /// <summary>
+        /// 异常地图块消失
+        /// </summary>
+        /// <param name="vector2"></param>
+        public void SetBattleMapBlockNormal(Vector2 vector2)
+        {
+            BattleMapBlock bm = null;
+            bm = BattleMap.Instance().GetSpecificMapBlock(vector2);
+            bm.blockType = EMapBlockType.normal;
+            SpriteRenderer upLayer = bm.transform.Find("upLayer").GetComponent<SpriteRenderer>();
+            upLayer.color = new Color(255, 255, 255, 0);
+        }
+
         //单位进入灼烧块
         public void UnitEnterBurning(Vector2 vector2)
         {
