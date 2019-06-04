33778c4d40ea05c5dab8c434a61f2afc290734af
13718982155 <35959757+13718982155@users.noreply.github.com>
2019-06-05 00:46:08 +0800

    1.新增事件：释出蜂群 2.事件基类添加相关新方法

--- GloryFantasy/Assets/Scripts/EventScroll/Event/Chomper_SummonBees.cs
+++ GloryFantasy/Assets/Scripts/EventScroll/Event/Chomper_SummonBees.cs
@@ -6,12 +6,12 @@ using UnityEngine;
 namespace GamePlay.Event
 {
 
-    public class Hostling : Event
+    public class Chomper_SummonBees : Event
     {
-        public Hostling()
+        public Chomper_SummonBees()
         {
             //从数据库读取属性，id名不能错
-            EventDataBase.Instance().GetEventProperty("Hostling", this);
+            EventDataBase.Instance().GetEventProperty("Chomper_SummonBees", this);
             //实例化该事件的 触发条件 和 效果
             this.Condition = selfCondition;
             this.Action = selfAction;
@@ -26,14 +26,15 @@ namespace GamePlay.Event
 
         void selfAction()
         {
-            //"我方立即抽一张牌。",
+            //"在来源单位周围部署Y个迅雷毒蜂（CR 1）。
 
             int source_type_flag = this.Get_Source_Message();       //函数内部进行信息获取。返回值用于错误控制
                                                                     //X次效果 最终值为读取的初始值与delta值的加和
             this.amount += delta_x_amount;
             //Y为效果强度 最终值为读取的初始值与delta值的加和
             this.strenth += delta_y_strenth;
-            IMessage.MsgDispatcher.SendMsg((int)IMessage.MessageType.DrawCard);     //发送抽卡指令
+            //在源单位的周围放置制定怪物
+            SummonMonster_in_Unit_Around(this.Source, this.strenth, "Bumblebee_1");
 
         }
     }
