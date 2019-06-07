9c7e5f9ac0b8ffb54598017382feeb7553f54df5
13718982155 <35959757+13718982155@users.noreply.github.com>
2019-06-07 18:57:11 +0800

    增加 攻击力上升1 的buff

---
+++ GloryFantasy/Assets/Scripts/Ability/Buff/Attackup1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Ability.Buff;
+
+namespace Ability.Buff
+{
+    public class AttackUp1 : Buff
+    {
+        //设定Buff的初始化
+        public override void InitialBuff()
+        {
+            //设定Buff的生命周期，两种写法,建议使用第二种，比较直观
+            //this.Life = 2f;
+            SetLife(1f);
+
+            //Buff要做的事情，可以像Ability一样也写Trigger，也可以只是做一些数值操作。和Ability一样公用一套工具函数库
+            GameUnit.GameUnit unit = GetComponent<GameUnit.GameUnit>();
+            //unit.MaxHP += 2;
+            //unit.hp += 2;
+            unit.changeATK(1);
+        }
+
+        //设定Buff消失时的逆操作
+        protected override void OnDisappear()
+        {
+            GetComponent<GameUnit.GameUnit>().changeATK(-1);
+        }
+    }
+}
