82512b11bd68801b19633a4beb4c4a4157ba0851
Fifholz <Fifholz@Github>
2019-06-23 15:48:45 +0800

    修复了城墙术

--- GloryFantasy/Assets/Scripts/Ability/Skill/Crash.cs
+++ GloryFantasy/Assets/Scripts/Ability/Skill/Crash.cs
@@ -1,37 +1,79 @@
 using GamePlay;
 using IMessage;
 using Mediator;
+using System.Collections.Generic;
 
 namespace Ability
 {
-    
+    public class Crash : Ability
+    {
+        Trigger trigger;
+        public override void Init(string abilityId)
+        {
+            base.Init(abilityId);
+            trigger = new TCrash(GetComponent<GameUnit.GameUnit>().GetMsgReceiver());
+            MsgDispatcher.RegisterMsg(trigger, abilityId);
+        }
+    }
+
+    public class TCrash : Trigger
+    {
+        public TCrash(MsgReceiver _speller)
+        {
+            register = _speller;
+            msgName = (int)MessageType.Summon;
+            condition = Condition;
+            action = Action;
+        }
+
+        private bool Condition()
+        {
+            //获取召唤列表
+            List<GameUnit.GameUnit> SummonUnits = this.GetSummonUnit();
+            //循环查询有没有召唤的怪是这个技能的发动者
+            for (int i = 0; i < SummonUnits.Count; i++)
+            {
+                if (SummonUnits[i].GetMsgReceiver() == register)
+                    return true;
+            }
+            return false;
+        }
+
+        private void Action()
+        {
+            //获取发动这个技能的怪
+            List<GameUnit.GameUnit> SummonUnits = this.GetSummonUnit();
+            GameUnit.GameUnit unit = null;
+            for (int i = 0; i < SummonUnits.Count; i++)
+            {
+                if (SummonUnits[i].GetMsgReceiver() == register)
+                    unit = SummonUnits[i];
+            }
+            unit.gameObject.AddBuff<BCrash>(2f);
+        }
+    }
     /// <summary>
     /// 此单位会于部署后{Turns}消解。
     /// </summary>
-    public class Crash : Buff.Buff
+    public class BCrash : Buff.Buff
     {
-        private Trigger _trigger;
+        public override void InitialBuff()
+        {
+            //设定Buff的生命周期，两种写法,建议使用第二种，比较直观
+            //this.Life = 2f;
+            SetLife(2f);
 
-//        public override void Init(string abilityId)
-//        {
-//            base.Init(abilityId);
-//            string id = GetComponent<GameUnit.GameUnit>().id;
-//
-//            //_trigger = new DelayedTrigger(
-//            //    this.GetUnitReceiver(this),
-//            //    AbilityVariable.Turns.Value,
-//            //    (int)MessageType.MPEnd,
-//            //    () =>
-//            //    {
-//            //        AbilityMediator.Instance().SendUnitToDeath(id);
-//            //    }
-//            //    );
-//            //    MsgDispatcher.RegisterMsg(_trigger, abilityId + "--DT", true);
-//            int turn = GameplayToolExtend.getTurnNum();
-//            
-//
-//            
-//        }
-        
+            //Buff要做的事情，可以像Ability一样也写Trigger，也可以只是做一些数值操作。和Ability一样公用一套工具函数库
+            
+            //unit.MaxHP += 2;
+            //unit.hp += 2;
+        }
+
+        //设定Buff消失时的逆操作
+        public override void OnComplete()
+        {
+            GameUnit.GameUnit unit = GetComponent<GameUnit.GameUnit>();
+            GameUnit.UnitManager.Kill(null, unit);
+        }
     }
 }
