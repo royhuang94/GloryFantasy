ee1a3d9967a0120372a2af6c46212141d70acc58
Fifholz <Fifholz@Github>
2019-06-25 16:35:41 +0800

    修复了翠林旅伴的治疗异能

--- GloryFantasy/Assets/Scripts/Input/FSM/InputFSMSummonState.cs
+++ GloryFantasy/Assets/Scripts/Input/FSM/InputFSMSummonState.cs
@@ -31,16 +31,16 @@ namespace GamePlay.FSM
             //把这张手牌从手牌里删掉
             CardManager.Instance().RemoveCardToMapList(FSM.selectedCard.gameObject);
             // 扣除消耗的Ap值
-            Player.Instance().ConsumeAp(FSM.selectedCard.GetComponent<BaseCard>().cost);  
+            Player.Instance().ConsumeAp(FSM.selectedCard.GetComponent<BaseCard>().cost);
+            //关闭鼠标所在战区的高光显示
+            BattleMap.BattleMap.Instance().IsColor = false;
+            //状态机压入静止状态
+            this.FSM.PushState(new InputFSMIdleState(FSM));
             //执行部署指令在对应MapBlock生成单位
             DispositionCommand unitDispose = new DispositionCommand(FSM.selectedCard.tokenID, OwnerEnum.Player, mapBlock);
             unitDispose.Excute();
             //删掉对应手牌槽的引用
             FSM.selectedCard = null;
-            //关闭鼠标所在战区的高光显示
-            BattleMap.BattleMap.Instance().IsColor = false;
-            //状态机压入静止状态
-            this.FSM.PushState(new InputFSMIdleState(FSM));
         }
 
         public override void OnPointerEnter(BattleMapBlock mapBlock, PointerEventData eventData)
