80615790c2fe8a163888803dc33f76e23a251fe0
Fifholz <Fifholz@Github>
2019-07-13 14:49:25 +0800

    数据库格式修改

--- GloryFantasy/Assets/Scripts/Mediator/Deck.cs
+++ GloryFantasy/Assets/Scripts/Mediator/Deck.cs
@@ -5,33 +5,15 @@ using System.Text;
 
 namespace Mediator
 {
+    
     public class Deck
     {
-        private List<string> _qpUnit = new List<string> { "HKnight_3", "HLunamage_3" };
-        public List<string> _deck; // 牌库中所有牌的id
-        public string _hero; // 主角牌id
-        public List<string> _unitsWithQuickPlat; // 具有即时备战的单卡
-
-        public Deck(List<string> deck, string hero)
+        public List<HeroData> _heros;
+        public class HeroData
         {
-            _hero = hero;
-            FreshDeck(deck);
-            deck.Remove(hero);
-            //deck.Add("")
-        }
-        public void FreshDeck(List<string> deck)
-        {
-            _deck = deck;
-            _unitsWithQuickPlat = new List<string> { _hero };
-            for (int i = 0; i < _deck.Count; i++)
-            {
-                if (_qpUnit.Contains(_deck[i]))
-                {
-                    _unitsWithQuickPlat.Add(_deck[i]);
-                    _deck.Remove(_deck[i--]);
-                }
-            }
-
+            string id;
+            List<string> additionalBuff;
+            List<string> arks;
         }
     }
 }
